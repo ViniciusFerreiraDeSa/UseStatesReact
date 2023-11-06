6018d9fbd5e024e716b9e595d69667e4470da040
Vinicius Ferreira de Sá <125524394+ViniciusFerreiraDeSa@users.noreply.github.com>
2023-11-06 14:48:12 -0300

    Estudo de UseState

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState } from 'react';
+
+function App() {
+  
+  const [endereco, setEndereco] = useState({});
+
+ 
+  function manipularEndereco(evento){
+    const cep = evento.target.value
+
+    setEndereco({ 
+      cep
+     })
+     if(cep && cep.length === 8){
+      //Obter o cep
+      fetch(`https://viacep.com.br/ws/${cep}/json/`)
+      .then(resposta => resposta.json())
+      .then(dados => {
+        setEndereco(enderecoAntigo => {
+          return{
+            ...enderecoAntigo,
+            bairro: dados.bairro,
+            cidade: dados.localidade,
+            estado: dados.uf,
+          }
+        })
+      })
+     }
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <input 
+        placeholder='Digite o CEP' 
+        onChange={manipularEndereco}/>
+        <ul>
+          <li>CEP: {endereco.cep}</li>
+          <li>Bairro: {endereco.bairro}</li>
+          <li>Cidade: {endereco.cidade}</li>
+          <li>Estado: {endereco.uf}</li>
+          
+        </ul>
+      </header>
+    </div>
+  );
+}
+
+export default App;
